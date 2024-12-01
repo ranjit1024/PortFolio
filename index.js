083ee2ecad18d17a9cafd9128ba31033eba459a5
ranjit1024 <ranjitdas2048@gmail.com>
2024-12-01 17:49:15 +0530

    adding basic animations

---
+++ index.js
@@ -0,0 +1,13 @@
+// nav Animation
+gsap.from(".nav-button", {
+    y:-100,
+    duration:1,
+    opacity:0,
+    stagger:1
+})
+gsap.from("ul button", {
+    x:100,
+})
+gsap.from("img", {
+    x:-100,
+})
