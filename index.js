94f4acecde108b7adc75b060295066c467509bcb
ranjit1024 <ranjitdas2048@gmail.com>
2024-12-08 16:10:55 +0530

    fixed the bug

--- index.js
+++ index.js
@@ -33,85 +33,89 @@ function breakTextAccordingToSpace(){
 
 
 breakTextAccordingToSpace()
-gsap.from(".nav-button", {
-    y:-100,
-    duration:1,
-    opacity:0,
-    stagger:1
-})
-gsap.from("ul button", {
-    x:100,
-})
-gsap.from("nav img", {
-    x:-100,
-})
+// gsap.from(".nav-button", {
+//     y:-100,
+//     duration:1,
+//     opacity:0,
+//     stagger:1
+// })
+// gsap.from("ul button", {
+//     x:100,
+// })
+// gsap.from("nav img", {
+//     x:-100,
+// })
 
-timeLine.from("main .greeting", {
-    duration:0.77,
-    opacity:0,
-    transform:"rotate(-7deg)",
-    y:10,
-    color:"white"
-})
+// timeLine.from("main .greeting", {
+//     duration:0.77,
+//     opacity:0,
+//     transform:"rotate(-7deg)",
+//     y:10,
+//     color:"white"
+// })
 
 
 
-timeLine.from(".sub-title span", {
-    duration:0.77,
-    x:1000,
-    opacity:0,
-    stagger:0.3,
-})
+// timeLine.from(".sub-title span", {
+//     duration:0.77,
+//     x:1000,
+//     opacity:0,
+//     stagger:0.3,
+// })
 
-timeLine.to(".overlay", {
-    duration:0.5,
-    opacity:0.3,
-    width:"100%"
-});
+// timeLine.to(".overlay", {
+//     duration:0.5,
+//     opacity:0.3,
+//     width:"100%"
+// });
 
-timeLine.from(".about", {
-    duration:0.77,
-    opacity:0,
-    y:100,
-})
+// timeLine.from(".about", {
+//     duration:0.77,
+//     opacity:0,
+//     y:100,
+// })
 
-timeLine.from("main button", {
-    scale:1.2,
-    opacity:0,
-    Color:"red",
-})
+// timeLine.from("main button", {
+//     scale:1.2,
+//     opacity:0,
+//     Color:"red",
+// })
 
-gsap.from(".icon img", {
-    y:-10,
-    duration:0.44,
-    opacity:0,
-    stagger:0.77,
-})
+// gsap.from(".icon img", {
+//     y:-10,
+//     duration:0.44,
+//     opacity:0,
+//     stagger:0.77,
+// })
 
-timeLine2.from(".tech-stack ul .tech-image", {
+timeLine2.from(".tech-image", {
     duration:15,
     opacity:0,
-    x:25,
+    x:12,
     scale:1.2,
     rotate:6,
     stagger:3,
+    overflow:"hidden",
     scrollTrigger:{
         trigger:".tech-stack",
         scroller:"body",
         // markers:true,
         start:"-20% start",
-        end:"+=1000",
+        end:"+=400",
         scrub:2,
         pin:true,
+        
     }
     
 })
+
 timeLine2.from(".project1", {
     duration:15,
     opacity:0,
     y:100,
     scale:2,
     rotate:10,
+    overflow:"hidden",
     scrollTrigger:{
         trigger:".projects",
         scroller:"body",
@@ -122,5 +126,3 @@ timeLine2.from(".project1", {
         pin:true,
     }
 })
-
-
